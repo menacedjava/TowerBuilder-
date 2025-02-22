fecff221192861a175a55f0b07e72b2c53ce3dc9
Madumarov Davron <menaceteacher@gmail.com>
2025-02-22 15:50:59 +0500

    add

--- src/main/java/org/example/TowerBuilder.java
+++ src/main/java/org/example/TowerBuilder.java
@@ -83,19 +83,19 @@ public class TowerBuilder extends JPanel implements ActionListener, KeyListener 
         frame.setLocationRelativeTo(null);
     }
 }
-//
-//class Block {
-//    int x, y, width, height;
-//
-//    public Block(int x, int y, int width, int height) {
-//        this.x = x;
-//        this.y = y;
-//        this.width = width;
-//        this.height = height;
-//    }
-//
-//    public void draw(Graphics g) {
-//        g.setColor(Color.BLUE);
-//        g.fillRect(x, y, width, height);
-//    }
+
+class Block {
+    int x, y, width, height;
+
+    public Block(int x, int y, int width, int height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+    public void draw(Graphics g) {
+        g.setColor(Color.BLUE);
+        g.fillRect(x, y, width, height);
+    }
 }
